2ba11062a62a4239e46ca6c951ae19fcddef3fb6
Juan Paez <juan.davidpaez@hotmail.com>
2024-07-09 00:01:51 +0200

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
+public class HomeController : Controller
+{
+	public IActionResult Index()
+{
+    return View();
+}
+
+    [HttpPost]
+    public IActionResult CreateDoc(string title, string subtitle, string paragraph, string footer)
+    {
+        using (var doc = DocX.Create("Documento.docx"))
+        {
+            // Crear el título
+            var titleFormat = new Formatting();
+            titleFormat.Size = 18D;
+            titleFormat.Position = 12;
+            titleFormat.Bold = true;
+
+            doc.InsertParagraph(title, false, titleFormat);
+
+            // Crear el subtítulo
+            var subtitleFormat = new Formatting();
+            subtitleFormat.Size = 14D;
+            subtitleFormat.Position = 10;
+            subtitleFormat.Italic = true;
+
+            doc.InsertParagraph(subtitle, false, subtitleFormat);
+
+            // Crear el párrafo
+            doc.InsertParagraph(paragraph);
+
+            // Crear el pie de página
+            var footerFormat = new Formatting();
+            footerFormat.Size = 10D;
+            footerFormat.Position = 10;
+
+            doc.AddFooters();
+            doc.Footers.Even.InsertParagraph(footer, false, footerFormat);
+            doc.Footers.Odd.InsertParagraph(footer, false, footerFormat);
+
+            // Guardar el documento
+            doc.Save();
+        }
+
+        return View();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews(); // Añade los servicios necesarios para usar controladores y vistas
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
